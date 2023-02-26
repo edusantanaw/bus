b52ee2982a22eea3cc30af13893447cc8abda031
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-02-26 13:16:28 -0300

    refactory:  load place controller

---
+++ src/main/java/com/edusantanaw/bus/presentation/controllers/CreatePlaceController.java
@@ -0,0 +1,33 @@
+package com.edusantanaw.bus.presentation.controllers;
+
+import com.edusantanaw.bus.domain.entities.Place;
+import com.edusantanaw.bus.domain.usecases.ICreatePlace;
+import com.edusantanaw.bus.domain.usecases.ILoadPlace;
+import com.edusantanaw.bus.validation.DTO.CreatePlaceDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/place")
+public class CreatePlaceController {
+    private final ICreatePlace createPlace;
+    private ILoadPlace loadPlace;
+
+    @PostMapping
+    public ResponseEntity<?> create(
+            @RequestBody @Valid CreatePlaceDTO data
+    ) {
+        try {
+            Place place = createPlace.execute(data);
+            return new ResponseEntity<>(place, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
--- src/main/java/com/edusantanaw/bus/presentation/controllers/LoadPlacesController.java
+++ src/main/java/com/edusantanaw/bus/presentation/controllers/LoadPlacesController.java
@@ -1,15 +1,15 @@
 package com.edusantanaw.bus.presentation.controllers;
 
 import com.edusantanaw.bus.domain.entities.Place;
-import com.edusantanaw.bus.domain.usecases.ICreatePlace;
 import com.edusantanaw.bus.domain.usecases.ILoadPlace;
-import com.edusantanaw.bus.validation.DTO.CreatePlaceDTO;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.print.Pageable;
 import java.util.List;
@@ -18,23 +18,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/place")
-public class PlaceController {
-    private final ICreatePlace createPlace;
+public class LoadPlacesController {
     private ILoadPlace loadPlace;
-
-    @PostMapping
-    public ResponseEntity<?> create(
-            @RequestBody @Valid CreatePlaceDTO data
-    ) {
-        try {
-            Place place = createPlace.execute(data);
-            return new ResponseEntity<>(place, HttpStatus.CREATED);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
     @GetMapping
     public ResponseEntity<List<Place>> loadAll(Pageable page) {
         List<Place> places = loadPlace.loadAllPlaces(page);
@@ -56,4 +41,5 @@ public class PlaceController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+
 }
