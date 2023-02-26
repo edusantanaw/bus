80bbea2e265540543788f7a439eb7df7c4eac7cb
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-02-26 12:05:14 -0300

    feat: load place interface

---
+++ src/main/java/com/edusantanaw/bus/domain/usecases/ILoadPlace.java
@@ -0,0 +1,13 @@
+package com.edusantanaw.bus.domain.usecases;
+
+import com.edusantanaw.bus.domain.entities.Place;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.UUID;
+
+public interface ILoadPlace {
+    Place loadById(UUID id);
+    List<Place> loadPlacesByName(String name);
+    List<Place> loadAllPlaces(Pageable page);
+}
